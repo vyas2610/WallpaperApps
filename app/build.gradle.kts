a04f3b7b497b344868f9dd5bd8f120fe75c256e6
vyas2610 <deepakvyas261092@gmail.com>
2024-11-08 23:14:54 +0530

    Splash Screen Added..!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+
 }
 
 android {
@@ -32,12 +33,15 @@ android {
 }
 
 dependencies {
-
+    val lottiefiles ="6.6.0"
+    implementation("com.airbnb.android:lottie:$lottiefiles")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
---
+++ app/src/main/java/com/example/wallpaperapps/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.wallpaperapps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
